59ba96523d08ea90cbc123fe62869e3a13dd69c1
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-19 16:32:06 +0100

    Add javascript to handle scrolling effects

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const win = window,
+    docEl = document.documentElement,
+    topNavbar = document.getElementById('nav-top'),
+    logo = document.getElementById('logo'),
+    navBottomText = document.getElementById('nav-bottom-text');
+
+win.onscroll = function(){
+  const sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
+  if(sTop > 10) {
+    topNavbar.style.display = "none";
+    logo.style.display = "inline-block";
+    logo.style.padding = "10px 20px";
+    logo.style.height = "25px";
+    navBottomText.style.margin = "0 0 0 130px";
+  } else {
+    topNavbar.style.display = null;
+    logo.style.display = null;
+    logo.style.padding = null;
+    logo.style.height = null;
+    navBottomText.style.margin = null;
+  }
+};
