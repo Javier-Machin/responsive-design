6e76741e7040b1702e8f2a9d55a945666b6b2f62
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-19 22:46:27 +0100

    Add logo text transition

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const win = window,
     docEl = document.documentElement,
     topNavbar = document.getElementById('nav-top'),
     logo = document.getElementById('logo'),
+    logoText = document.getElementById('logo-text'),
     navBottomText = document.getElementById('nav-bottom-text');
 
 win.onscroll = function(){
@@ -11,12 +12,15 @@ win.onscroll = function(){
     logo.style.display = "inline-block";
     logo.style.padding = "7px 20px";
     logo.style.height = "34px";
+    setTimeout(function() { logoText.style.top = "3px";}, 100);
+    
     navBottomText.style.margin = "0 0 0 130px";
   } else {
     topNavbar.style.display = null;
     logo.style.display = null;
     logo.style.padding = null;
     logo.style.height = null;
+    logoText.style.top = "-10px";
     navBottomText.style.margin = null;
   }
 };
