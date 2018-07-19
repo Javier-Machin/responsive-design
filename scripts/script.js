33f499183946beb8060c5b5e992b54d3f990fbfa
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-19 21:36:32 +0100

    Refactor CSS

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,8 @@ win.onscroll = function(){
   if(sTop > 10) {
     topNavbar.style.display = "none";
     logo.style.display = "inline-block";
-    logo.style.padding = "10px 20px";
-    logo.style.height = "25px";
+    logo.style.padding = "7px 20px";
+    logo.style.height = "34px";
     navBottomText.style.margin = "0 0 0 130px";
   } else {
     topNavbar.style.display = null;
