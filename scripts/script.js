01fc500bb7609128e5f3048cb013247e9f04144a
Javier Machin <javier_machin_acosta@hotmail.com>
2018-07-20 17:47:21 +0100

    Add header

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,18 @@
 const win = window,
-    docEl = document.documentElement,
-    topNavbar = document.getElementById('nav-top'),
-    logo = document.getElementById('logo'),
-    logoText = document.getElementById('logo-text'),
-    navBottomText = document.getElementById('nav-bottom-text');
+      docEl = document.documentElement,
+      topNavbar = document.getElementById('nav-top'),
+      logo = document.getElementById('logo'),
+      logoText = document.getElementById('logo-text'),
+      navBottomText = document.getElementById('nav-bottom-text');
 
 win.onscroll = function(){
-  const sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
+  const sTop = (this.pageYOffset || docEl.scrollTop) - (docEl.clientTop || 0);
   if(sTop > 10) {
     topNavbar.style.display = "none";
     logo.style.display = "inline-block";
     logo.style.padding = "7px 20px";
     logo.style.height = "34px";
     setTimeout(function() { logoText.style.top = "3px";}, 100);
-    
     navBottomText.style.margin = "0 0 0 130px";
   } else {
     topNavbar.style.display = null;
